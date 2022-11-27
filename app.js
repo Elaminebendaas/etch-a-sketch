f1e5b31c268546e02e3340dd8675801969d00294
ElamineBendaas <33202205+Elaminebendaas@users.noreply.github.com>
2022-11-27 17:25:13 -0500

    made grid work

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const body = document.body
-const drawBox = document.getElementById('draw-box')
-const colorMode = document.getElementById('draw')
-const eraseMode = document.getElementById('erase')
-let drawMode = true
+const body = document.body;
+const drawBox = document.querySelector(".draw-box");
+const colorMode = document.getElementById("draw");
+const eraseMode = document.getElementById("erase");
+let drawMode = true;
 colorMode.onclick = () => {
-    drawMode = true
-}
+  drawMode = true;
+};
 eraseMode.onclick = () => {
-    drawMode = false
-}
-
-
-
+  drawMode = false;
+};
 
 for (let index = 0; index < 256; index++) {
-    var drawDiv = document.createElement('div')
-    drawDiv.setAttribute('class', 'clear-draw')
-    drawBox.append(drawDiv)
-    console.log(index)
-}
+  var drawDiv = document.createElement("div");
+  drawDiv.setAttribute("class", "clear-draw");
+  drawDiv.id = `${index}`;
+  drawBox.append(drawDiv);
 
-drawDiv.onclick = () =>{
-    if(drawMode == true){
-        drawDiv.classList.replace('clear-draw', 'draw')
-    }else{
-        drawDiv.classList.replace('draw', 'clear-draw')
+  drawDiv.onclick = (event) => {
+    const blockId = event.path[0].id;
+    const blockIdSelect = document.getElementById(`${blockId}`);
+
+    if (drawMode == true) {
+      blockIdSelect.style.backgroundColor = "black";
+    } else {
+      blockIdSelect.style.backgroundColor = "white";
     }
+  };
 }
